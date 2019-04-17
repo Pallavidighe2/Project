c157cdc791f66c7e08b44f32cb2141f27bd2fd2d
pallavi <pallavidighe2@gmail.com>
2019-04-17 20:29:11 +0530

    Income Tax Calculator

--- Income Tax Calculator.py
+++ Income Tax Calculator.py
@@ -1,5 +1,7 @@
 def income_tax_calculaor():
-
+    global Total_Income
+    Total_Income = input("Enter your Total Annual income :  ")
+    Total_Income = int(Total_Income)
     investment()
 
 
@@ -13,8 +15,8 @@ def investment():
 
 
 def tax_calculate_without_deduction():
-    Total_Income = input("Enter your Total Annual income :  ")
-    Total_Income = int(Total_Income)
+
+
     if Total_Income <= 250000:
         print("You don't pay any tax ")
     elif 250001 <= Total_Income <= 500000:
@@ -37,17 +39,32 @@ def tax_calculate_without_deduction():
 def tax_calculate_with_deduction():
 
     deduction()
-
-
-
-
+    taxable_income =  Total_Income-Total_Deduction
+    print(taxable_income)
+    if taxable_income <= 250000:
+        print("You don't pay any tax ")
+    elif 250001 <= taxable_income <= 500000:
+        A = taxable_income - 250000
+        Tax1 = A * 0.05
+        print("you have to pay Tax1 {}".format(Tax1))
+    elif 500001 <= taxable_income <= 1000000:
+        A = taxable_income - 250000
+        B = A - 250000
+        Tax2 = (B * 0.2) + 12500
+        print("you have to pay Tax1 {}".format(Tax2))
+    elif taxable_income >= 1000000:
+        A = taxable_income - 250000
+        B = A - 250000
+        C = B - 500000
+        Tax3 = (C * 0.3) + 100000 + 12500
+        print("you have to pay Tax1 {}".format(Tax3))
 def deduction():
-    addition = sum + NPS + medicalim
-    print(addition)
-    Deduction_Under_80C()
+    global Total_Deduction
     Deduction_Under_80CCD()
+    Deduction_Under_80C()
     Deduction_for_medicalim()
-
+    Total_Deduction = x + NPS + mediclaim
+    print(Total_Deduction)
 
 
 def Deduction_Under_80C():
@@ -75,18 +92,20 @@ def Deduction_Under_80C():
             Residential_Housing_Loan, Sukanya_Samrudhhi_Scheme]
 
     print(list)
-
-    sum = 0
+    global x
+    x = 0
     for num in list:
-        sum = sum + int(num)
-    print("Sum = ", sum)
-    if sum <=150000:
-        print(sum)
+        x = x + int(num)
+    print("x = ", x)
+    if x <=150000:
+        print(x)
     else:
-        sum=150000
-        print(sum)
+        x=150000
+        print(x)
 
 def Deduction_Under_80CCD():
+
+    global NPS
 
     print("Additional deduction under 80CCD 50,000")
 
@@ -98,6 +117,7 @@ def Deduction_Under_80CCD():
         NPS = 50000
         print(NPS)
 def Deduction_for_medicalim():
+    global  mediclaim
     print("investment under section 80 D max 15,000")
     mediclaim = int(input("Enter your mediclaim amount: "))
     if mediclaim <= 15000:
@@ -110,51 +130,54 @@ def Deduction_for_medicalim():
 
 income_tax_calculaor()
 
-"""
-    if Total_Income <= 250000:
-        print("You don't pay any tax ")
-
-    elif 250001 <= Total_Income <= 500000:
-
-        A = Total_Income - 250000
-
-        A=int(A)
-
-        if  A > 150000:
-
-            Bank_FD = input("Enter the amount you invest for Mutual_Fund : ")
-
-            Bank_FD = int(Bank_FD)
 
 
-            B = A- Bank_FD
 
-            Tax1 = B * 0.05
 
-            print("you have to pay Tax1 {}".format(Tax1))
 
-        else:
 
-            Tax2 = A * 0.05
 
-            print("you have to pay Tax2 {}".format(Tax2))
 
-    elif Bank_FD < 150000:
 
-        B = A - Bank_FD
 
-        Tax2 = B * 0.05
 
-        print("you have to pay Tax1 {}".format(Tax2))
-"""
-# (LIC=input("Enter the amount you invest for LIC : ")
-# National_Saving_Certificate = input("Enter the amount you invest for National_Saving_Certificate : ")
-# Invetsment_In_PF = input("Enter the amount you invest for Invetsment_In_PF : ")
-# Tuition_Fee = input("Enter the amount you invest for Tuition_Fee : ")
-# Mutual_Fund = input("Enter the amount you invest for Mutual_Fund : ")
-# Bank_FD = input("Enter the amount you invest for Mutual_Fund : ")
-# House_Loan_Repayment = input("Enter the amount you invest for House_Loan_Repayment : ")
-# Employee_PF = input("Enter the amount you invest for Employee_PF : ")
-# Stamp_Duty = input("Enter the amount you invest for Stamp_Duty : ")
-# Residential_Housing_Loan = input("Enter the amount you invest for Residential_Housing_Loan : ")
-# Sukanya_Samrudhhi_Scheme = input("Enter the amount you invest for Sukanya_Samrudhhi_Scheme : ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
